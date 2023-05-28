0815b004290120e5bd11169de9c4c46f7b572115
Deepthi pPiyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2023-05-28 11:00:06 +0530

    n=int(input()) c=[] for i in range(1,n+1):     c.append(str(i))     print("".join(c))

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,5 @@
 n=int(input())
-m={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
+c=[]
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(m[i],end=" ")
-    print()    
+    c.append(str(i))
+    print("".join(c))
