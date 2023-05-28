e06c4594613f2c799ad23de4b47633986656919e
Deepthi pPiyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2023-05-28 14:42:05 +0530

    s=input() s=s.split(":") s=list(s) m=int(s[-1])*6 h=((int(s[-2])*60)+int(s[-1]))*0.5 if abs(h-m)>180:     print(360-abs(h-m)) else:     print(h-m)

--- Angle_Between_Hands.py
+++ Angle_Between_Hands.py
@@ -1,7 +1,9 @@
 s=input()
-h=int(s[:2])
-m=int(s[3:])
-s=abs(30*h-5.5*m)
-if(s>180):
-    s=360-s
-print(s)    
+s=s.split(":")
+s=list(s)
+m=int(s[-1])*6
+h=((int(s[-2])*60)+int(s[-1]))*0.5
+if abs(h-m)>180:
+    print(360-abs(h-m))
+else:
+    print(h-m)
